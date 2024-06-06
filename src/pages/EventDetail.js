1e50befdeba64b6f536e30906edfb10f943c75de
simonecapezzani <simonecapezzani@gmail.com>
2024-06-06 12:37:19 +0200

    Getting parameters in loader

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -1,15 +1,30 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData,json } from "react-router-dom";
+import EventItem from '../components/EventItem';
 
 function EventDetailPage() {
 
-    const params = useParams();
+    const data = useLoaderData();
 
     return (
-        <>
-            <h1>EventDetailPage</h1>
-            <h2>Event id: {params.eventId}</h2>
-        </>
+        <EventItem event={data.event} />
     );
 
 }
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+
+    const id = params.eventId;
+
+    const response = await fetch('http://localhost:8080/events/' + id);
+
+    if (!response.ok) {
+        return json(
+            { message: 'Could not fetch details' },
+            { status: 500 })
+    } else {
+        return response;
+    }
+
+
+}
