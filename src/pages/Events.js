3abf9922dd891abed05eeae5d7877daefd738b3a
simonecapezzani <simonecapezzani@gmail.com>
2024-06-11 09:58:33 +0200

    Implement defer on events load

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,26 +1,31 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { Suspense } from 'react';
+import { useLoaderData, json, defer, Await } from 'react-router-dom';
 import EventsList from '../components/EventsList';
 
 function EventsPage() {
 
-    const data = useLoaderData();
+    const { events } = useLoaderData();
 
-    if (data.isError) {
-        return <p>{data.message}</p>
-    }
-
-    const events = data.events;
+    // if (data.isError) {
+    //     return <p>{data.message}</p>
+    // }
 
     return (
-        <>
-            <EventsList events={events} />
-        </>
+        <Suspense fallback={
+            <p style={{ textAlign: 'center' }}>
+                Loading...
+            </p>
+        }>
+            <Await resolve={events}>
+                {(loadedEvents) => <EventsList events={loadedEvents} />}
+            </Await>
+        </Suspense>
     );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
     const response = await fetch('http://localhost:8080/events');
 
     if (!response.ok) {
@@ -36,7 +41,14 @@ export async function loader() {
             { status: 500 }
         );
     } else {
-        return response;
+        const resData = await response.json();
+        return resData.events;
     }
 
+}
+
+export function loader() {
+    return defer({
+        events: loadEvents()
+    });
 };
